62e02838da040826c8cbd9a5e144e5bcdbe622d1
Syed Shoeb Ullah Hussainy <61550309+Shoeb-08@users.noreply.github.com>
2022-05-23 20:30:27 +0530

    Delete react-blinkist-main directory

--- react-blinkist-main/src/components/organisms/BookTab/BookTab.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/react";
-import BookTab from "./BookTab";
-
-describe("Display Book Tab", () => {
-  test("Should be displayed", () => {
-    render(<BookTab />);
-  });
-});
